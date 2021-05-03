3cdc4fa4cbe7a500b1c72890a889d5a7b4bd980d
Sean Chen <Sean.Chen@leocorn.com>
2021-05-03 18:17:48 -0400

    try different characters for the border

--- border.py
+++ border.py
@@ -7,14 +7,16 @@ def tetris(stdscr):
     sh, sw = stdscr.getmaxyx()
 
     # find the border for game board.
-    border_ch = chr(127999)
+    #border_ch = chr(127999)
+    border_ch = chr(9634)
+    #border_ch = chr(9609)
 
     #stdscr.addstr(1, 0, border_ch)
     #stdscr.addstr(1, 3, border_ch)
 
     # paint the top and bottom border
-    for x in range(5, 39 + 5, 1):
-        stdscr.addstr(2, x, border_ch)
+    for x in range(5, 24 + 5, 2):
+        #stdscr.addstr(2, x, border_ch)
         stdscr.addstr(23, x, border_ch)
     # paint the left and right border
     for y in range(2, 24):
